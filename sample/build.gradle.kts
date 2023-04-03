352515583eddbd0609dff375634f510146377e55
seiko <seiko_des@outlook.com>
2023-04-03 11:23:51 +0800

    update sample

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("io.github.qdsfdhvh.tl-generator")
 }
 
 android {
@@ -66,4 +67,9 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+}
+
+gramTl {
+    packageName = "com.seiko.tlparser.sample.model"
+    prefix = "TL_"
 }
