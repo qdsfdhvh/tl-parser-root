3dca6e59778a2b575229fdd8350a712862881c8e
seiko <seiko_des@outlook.com>
2023-04-03 11:23:51 +0800

    add sample

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,3 +6,11 @@ pluginManagement {
     }
     includeBuild("tl-parser")
 }
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+include(":sample")
